da0eff367baa188957c15910f48036af2c5e93be
rushamd <schat5444@gmail.com>
2024-07-28 13:23:22 +0530

    Update repo.js

--- commandes/repo.js
+++ commandes/repo.js
@@ -2,8 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
-zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-  const githubRepo = 'https://api.github.com/repos/Cod3Uchiha/TKM-bot';
+zokou({ nomCom: "menu", catégorie:"Général", reaction: "✨", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
   const img = 'https://telegra.ph/file/e07a3d933fb4cad0b3791.jpg';
 
   try {
@@ -21,13 +20,230 @@ zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __f
       const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
       const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
-      const gitdata = `┏❏ ⌜ 𝐓𝐊𝐌-𝐁𝐎𝐓 𝐑𝐞𝐩𝐨 ⌟ ❐
-┃🗼 *REPOSITORY:* ${data.html_url}
-┃✨ *STARS:* ${repoInfo.stars}
-┃🧧 *FORKS:* ${repoInfo.forks}
-┃📅 *RELEASE DATE:* ${releaseDate}
-┃🕐 *UPDATE ON:* ${repoInfo.lastUpdate}
-┃👨‍💻 *OWNER* :Cod3Uchiha
+      const gitdata = `┏❏ ⌜ Rush ⌟ ❐
+┃👨‍💻 *OWNER* :Rush
+     ┏❏ *General*
+┃ ⎔ mods
+┃ ⎔ dev
+┃ ⎔ support
+┃ ⎔ menu2
+┃ ⎔ ping
+┃ ⎔ info
+┃ ⎔ runtime
+┃ ⎔ repo
+┃ ⎔ test
+┃ ⎔ testb
+┃ ⎔ tkminc
+┃ ⎔ channel
+┃ ⎔ vv
+┗❏
+┏❏ *Mods*
+┃ ⎔ afk
+┃ ⎔ boom
+┃ ⎔ left
+┃ ⎔ reboot
+┗❏
+┏❏ *Audio-Edit*
+┃ ⎔ deep
+┃ ⎔ bass
+┃ ⎔ reverse
+┃ ⎔ slow
+┃ ⎔ smooth
+┃ ⎔ tempo
+┃ ⎔ nightcore
+┗❏
+┏❏ *dev*
+┃ ⎔ bugmenu
+┃ ⎔ bug
+┃ ⎔ crash
+┃ ⎔ loccrash
+┃ ⎔ crashbug
+┃ ⎔ amountbug
+┃ ⎔ pmbug
+┃ ⎔ delaybug
+┃ ⎔ docubug
+┃ ⎔ unlimitedbug
+┃ ⎔ bombug
+┃ ⎔ lagbug
+┃ ⎔ trollybug
+┗❏
+┏❏ *Image-Edit*
+┃ ⎔ shit
+┃ ⎔ wasted
+┃ ⎔ wanted
+┃ ⎔ trigger
+┃ ⎔ trash
+┃ ⎔ rip
+┃ ⎔ sepia
+┃ ⎔ rainbow
+┃ ⎔ hitler
+┃ ⎔ invert
+┃ ⎔ jail
+┃ ⎔ affect
+┃ ⎔ beautiful
+┃ ⎔ blur
+┃ ⎔ circle
+┃ ⎔ facepalm
+┃ ⎔ greyscale
+┃ ⎔ joke
+┗❏
+┏❏ *Conversion*
+┃ ⎔ sticker
+┃ ⎔ scrop
+┃ ⎔ take
+┃ ⎔ write
+┃ ⎔ photo
+┃ ⎔ trt
+┃ ⎔ url
+┗❏
+┏❏ *Games*
+┃ ⎔ riddle
+┃ ⎔ rps
+┃ ⎔ quizz
+┗❏
+┏❏ *Group*
+┃ ⎔ welcome
+┃ ⎔ goodbye
+┃ ⎔ antipromote
+┃ ⎔ antidemote
+┃ ⎔ tagall
+┃ ⎔ link
+┃ ⎔ promote
+┃ ⎔ demote
+┃ ⎔ remove
+┃ ⎔ del
+┃ ⎔ ginfo
+┃ ⎔ antilink
+┃ ⎔ antibot
+┃ ⎔ group
+┃ ⎔ gname
+┃ ⎔ gdesc
+┃ ⎔ gpp
+┃ ⎔ hidetag
+┃ ⎔ automute
+┃ ⎔ autounmute
+┃ ⎔ fkick
+┃ ⎔ nsfw
+┃ ⎔ warn
+┗❏
+┏❏ *Fun*
+┃ ⎔ fancy
+┃ ⎔ profile
+┃ ⎔ quote
+┃ ⎔ rank
+┃ ⎔ toprank
+┗❏
+┏❏ *Research*
+┃ ⎔ apk
+┗❏
+┏❏ *Hentai*
+┃ ⎔ hwaifu
+┃ ⎔ trap
+┃ ⎔ hneko
+┃ ⎔ blowjob
+┃ ⎔ hentaivid
+┗❏
+┏❏ *TKM-bot vars*
+┃ ⎔ setvar
+┃ ⎔ getallvar
+┃ ⎔ getvar
+┗❏
+┏❏ *Download*
+┃ ⎔ igdl
+┃ ⎔ fbdl
+┃ ⎔ tiktok
+┃ ⎔ fbdl2
+┃ ⎔ ytmp4
+┃ ⎔ ytmp3
+┗❏
+┏❏ *Search*
+┃ ⎔ img
+┃ ⎔ lyrics
+┃ ⎔ stickersearch
+┃ ⎔ play
+┃ ⎔ video
+┃ ⎔ yts
+┗❏
+┏❏ *Logo*
+┃ ⎔ hacker
+┃ ⎔ dragonball
+┃ ⎔ naruto
+┃ ⎔ didong
+┃ ⎔ wall
+┃ ⎔ summer
+┃ ⎔ neonlight
+┃ ⎔ greenneon
+┃ ⎔ glitch
+┃ ⎔ devil
+┃ ⎔ explode
+┃ ⎔ water
+┃ ⎔ snow
+┃ ⎔ transformer
+┃ ⎔ thunder
+┃ ⎔ harrypotter
+┃ ⎔ cat
+┃ ⎔ whitegold
+┃ ⎔ lightglow
+┃ ⎔ thor
+┃ ⎔ neon
+┃ ⎔ purple
+┃ ⎔ gold
+┃ ⎔ arena
+┃ ⎔ incandescent
+┗❏
+┏❏ *Menu*
+┃ ⎔ menu
+┃ ⎔ menu3
+┗❏
+┏❏ *other*
+┃ ⎔ encode
+┗❏
+┏❏ *Reaction*
+┃ ⎔ bully
+┃ ⎔ cuddle
+┃ ⎔ cry
+┃ ⎔ hug
+┃ ⎔ awoo
+┃ ⎔ kiss
+┃ ⎔ lick
+┃ ⎔ pat
+┃ ⎔ smug
+┃ ⎔ bonk
+┃ ⎔ yeet
+┃ ⎔ blush
+┃ ⎔ smile
+┃ ⎔ wave
+┃ ⎔ highfive
+┃ ⎔ handhold
+┃ ⎔ nom
+┃ ⎔ bite
+┃ ⎔ glomp
+┃ ⎔ slap
+┃ ⎔ kill
+┃ ⎔ kick
+┃ ⎔ happy
+┃ ⎔ wink
+┃ ⎔ poke
+┃ ⎔ dance
+┃ ⎔ cringe
+┗❏
+┏❏ *stickcmd*
+┃ ⎔ setcmd
+┃ ⎔ delcmd
+┃ ⎔ allcmd
+┗❏
+┏❏ *tts*
+┃ ⎔ dit
+┃ ⎔ itta
+┃ ⎔ say
+┗❏
+┏❏ *Weeb*
+┃ ⎔ waifu
+┃ ⎔ neko
+┃ ⎔ shinobu
+┃ ⎔ megumin
+┃ ⎔ cosplay
+┃ ⎔ couplepp
 ┗❏`;
 
       await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
